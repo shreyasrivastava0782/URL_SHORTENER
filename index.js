e1bd9532aaabc93380dfc899488deeeb33445bb8
Shreya Srivastava <shreyasrivastava722@gmail.com>
2025-09-15 19:57:37 +0530

    replace hardcoded port with port using cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const Url = require("./db/url");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static("public"));
